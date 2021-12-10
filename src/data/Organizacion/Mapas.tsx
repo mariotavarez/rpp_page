7198a8eecafcb7394a8955181ff93e7d25d057c6
Mario Tavarez <mtavarez@queretaro.gob.mx>
2021-12-10 10:55:14 -0600

    RESPONSIVE COMMIT

--- src/data/Organizacion/Mapas.tsx
+++ src/data/Organizacion/Mapas.tsx
@@ -8,9 +8,9 @@ export const MAPAS_DATA: MapasModel[] = [
     subdireccion: "QUERÉTARO",
     nombreEncargado: "M. en D. Carlos Olguín González",
     img: "https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/personas/area_6_1.jpg",
-    telefono: "Teléfono: 442 227 1800 ext. 1851, 1852",
+    telefono: "442 227 1800 ext. 1851, 1852",
     ubicacion:
-      "Ubicación: Madero #68, Planta Alta, Centro Histórico, Santiago de Querétaro, Querétaro. C.P. 76000",
+      "Madero #68, Planta Alta, Centro Histórico, Santiago de Querétaro, Querétaro. C.P. 76000",
     src: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.9210825396044!2d-100.39809958507468!3d20.59128018623698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d35ad358bf725f%3A0x1b4583678d67b7d5!2sRegistro%20P%C3%BAblico%20de%20la%20Propiedad%20y%20del%20Comercio%20del%20Estado%20de%20Quer%C3%A9taro!5e0!3m2!1ses!2smx!4v1605570622930!5m2!1ses!2smx"
@@ -29,7 +29,7 @@ export const MAPAS_DATA: MapasModel[] = [
     img: "https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/personas/area_6_1.jpg",
     telefono: "442 227 1800 ext. 2101, 2104",
     ubicacion:
-      "Ubicación: Madero #70, Planta Alta, Centro Histórico, Santiago de Querétaro, Querétaro. C.P. 76000",
+      "Madero #70, Planta Alta, Centro Histórico, Santiago de Querétaro, Querétaro. C.P. 76000",
     src: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.9210825396044!2d-100.39809958507468!3d20.59128018623698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d35ad358bf725f%3A0x1b4583678d67b7d5!2sRegistro%20P%C3%BAblico%20de%20la%20Propiedad%20y%20del%20Comercio%20del%20Estado%20de%20Quer%C3%A9taro!5e0!3m2!1ses!2smx!4v1605570622930!5m2!1ses!2smx"
@@ -49,7 +49,7 @@ export const MAPAS_DATA: MapasModel[] = [
     img: "https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/personas/area_6_1.jpg",
     telefono: "442 227 1800 ext. 2124",
     ubicacion:
-      "Ubicación: Madero #68, Planta Alta, Centro Histórico, Santiago de Querétaro, Querétaro. C.P. 76000",
+      "Madero #68, Planta Alta, Centro Histórico, Santiago de Querétaro, Querétaro. C.P. 76000",
     src: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.9210825396044!2d-100.39809958507468!3d20.59128018623698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d35ad358bf725f%3A0x1b4583678d67b7d5!2sRegistro%20P%C3%BAblico%20de%20la%20Propiedad%20y%20del%20Comercio%20del%20Estado%20de%20Quer%C3%A9taro!5e0!3m2!1ses!2smx!4v1605570622930!5m2!1ses!2smx"
@@ -67,7 +67,7 @@ export const MAPAS_DATA: MapasModel[] = [
     nombreEncargado: "Lic. María Cecilia Ortiz Hernández",
     img: "https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/personas/area_6_1.jpg",
     telefono: "448 278 0120",
-    ubicacion: "Ubicación: Venustiano Carranza #57, Planta alta. C.P. 76850",
+    ubicacion: "Venustiano Carranza #57, Planta alta. C.P. 76850",
     src: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d936.1767638333242!2d-100.1454888!3d20.1879192!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6d538e2c961fbbce!2sRegistro%20P%C3%BAblico%20De%20La%20Propiedad%20Y%20Del%20Comercio!5e0!3m2!1ses!2smx!4v1605570567439!5m2!1ses!2smx"
@@ -86,7 +86,7 @@ export const MAPAS_DATA: MapasModel[] = [
     img: "https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/personas/area_6_1.jpg",
     telefono: "442 227 1800 ext. 2116",
     ubicacion:
-      "Ubicación: Madero #70, Planta Alta, Centro Histórico, Santiago de Querétaro, Querétaro. C.P. 76000",
+      "Madero #70, Planta Alta, Centro Histórico, Santiago de Querétaro, Querétaro. C.P. 76000",
     src: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.9210825396044!2d-100.39809958507468!3d20.59128018623698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d35ad358bf725f%3A0x1b4583678d67b7d5!2sRegistro%20P%C3%BAblico%20de%20la%20Propiedad%20y%20del%20Comercio%20del%20Estado%20de%20Quer%C3%A9taro!5e0!3m2!1ses!2smx!4v1605570622930!5m2!1ses!2smx"
@@ -143,7 +143,7 @@ export const MAPAS_DATA: MapasModel[] = [
     img: "https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/personas/area_6_1.jpg",
     telefono: "427 272 3224",
     ubicacion:
-      "Ubicación: Valentin Gómez Farias #7 int.118, Planta baja, Plaza Aguarica, Centro San Juan del Río, Qro. C.P. 76800",
+      "Valentin Gómez Farias #7 int.118, Planta baja, Plaza Aguarica, Centro San Juan del Río, Qro. C.P. 76800",
     src: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1869.925958918218!2d-99.9930519!3d20.3889946!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d30b06cc6e50bb%3A0xcabbabd4bd551032!2sRegistro%20P%C3%BAblico%20de%20la%20Propiedad!5e0!3m2!1ses!2smx!4v1605570652965!5m2!1ses!2smx"
@@ -162,7 +162,7 @@ export const MAPAS_DATA: MapasModel[] = [
     img: "https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/personas/area_6_1.jpg",
     telefono: "441 296 0559",
     ubicacion:
-      "Ubicación: Mariano Matamoros Esquina Abasolo, Planta alta, Colonia Centro, Jalpan de Serra, Qro. C.P. 76340",
+      "Mariano Matamoros Esquina Abasolo, Planta alta, Colonia Centro, Jalpan de Serra, Qro. C.P. 76340",
     src: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d929.853358110605!2d-99.47529807081185!3d21.215448199118583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d43e193b0a7d47%3A0xdb2bba94fed1a728!2sRegistro%20P%C3%BAblico%20de%20la%20Propiedad!5e0!3m2!1ses-419!2smx!4v1605570717437!5m2!1ses-419!2smx"
