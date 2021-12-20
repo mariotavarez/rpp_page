cc4bd8497279b7de80c2c006dd5326cb624adc49
Mario Tavarez <mtavarez@queretaro.gob.mx>
2021-12-20 15:44:56 -0600

    SKELETON: Se agrega skeleton a proyecto

--- src/components/shared/Screens/Carousel/Carousel.tsx
+++ src/components/shared/Screens/Carousel/Carousel.tsx
@@ -3,42 +3,74 @@ import "./Carousel.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
-export const CarouselComponent = () => {
-  return (
-    <Carousel
-      autoPlay={true}
-      infiniteLoop={true}
-      interval={5000}
-      showStatus={true}
-      showArrows={true}
-      axis={"horizontal"}
-      showIndicators={true}
-      autoFocus={true}
-      showThumbs={true}
-      useKeyboardArrows={true}
-      transitionTime={2000}
-      stopOnHover={true}
-      swipeable={true}
-    >
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import React from "react";
+// Hooks
+
+export class CarouselComponent extends React.Component<
+  {},
+  { loading: boolean }
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+  }
+
+  render() {
+    return !this.state.loading ? (
       <div>
-        <img
-          className="responsive-carousel"
-          alt="AVISO 1"
-          src="https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/avisos/aviso-oficial.png"
-        />
+        <SkeletonTheme baseColor="#b1b1b1" highlightColor="#7d7d7d">
+          <Skeleton count={5} height={30} />
+          <div className="container-loading-carousel">
+            <Skeleton count={1} width={100} />
+            <Skeleton count={1} width={100} />
+            <Skeleton count={1} width={100} />
+          </div>
+        </SkeletonTheme>
       </div>
-      <div>
-        <img
-          src="https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/avisos/copias-certificadas.png"
-          alt="AVISO 2"
-        />
-      </div>
-      <div>
-        <img
-          src="https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/avisos/cancelacion-hipoteca.png"
-          alt="AVISO 3"
-        />
-      </div>
-    </Carousel>
-  );
-};
+    ) : (
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        interval={5000}
+        showStatus={true}
+        showArrows={true}
+        axis={"horizontal"}
+        showIndicators={true}
+        autoFocus={true}
+        showThumbs={true}
+        useKeyboardArrows={true}
+        transitionTime={2000}
+        stopOnHover={true}
+        swipeable={true}
+      >
+        <div>
+          <img
+            className="responsive-carousel"
+            alt="AVISO 1"
+            src="https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/avisos/aviso-oficial.png"
+          />
+        </div>
+        <div>
+          <img
+            src="https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/avisos/copias-certificadas.png"
+            alt="AVISO 2"
+          />
+        </div>
+        <div>
+          <img
+            src="https://cerlin.ciasqro.gob.mx/app_assets/portal_assets/img/avisos/cancelacion-hipoteca.png"
+            alt="AVISO 3"
+          />
+        </div>
+      </Carousel>
+    );
+  }
+}
